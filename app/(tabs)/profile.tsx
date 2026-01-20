ba0ca0d1b73c97c439146b8b67f7c255376b995d
Mikiyas Kebede <mknewkebede@gmail.com>
2026-01-20 15:09:16 +0300

    implement movie detail

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,18 @@
+import { icons } from "@/constants/icons";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 const Profle = () => {
   return (
-    <View>
-      <Text>Profle</Text>
+    <View className="bg-primary flex-1 px-10">
+      <View className="flex items-center justify-center flex-1 flex-col gap-5">
+        <Image
+          source={icons.person}
+          className="size-10"
+          tintColor={"#FFFFFF"}
+        />
+        <Text className="text-gray-500 text-base">Profile</Text>
+      </View>
     </View>
   );
 };
